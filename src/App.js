1cb3e6ecda7b6c241925f0549167e66c5553071d
QuintonEL <quintonlaborde@gmail.com>
2021-05-13 08:05:19 -0600

    got the menu bar and sidebar in position

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import MenuBar from './components/MenuBar';
+import SideDrawer from './components/SIdeDrawer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+      <MenuBar />
+      <SideDrawer />
     </div>
   );
 }
